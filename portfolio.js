b0f98fc59aece24c53fa8b5b8a282ab38bcf6190
rajoriya12 <147902964+rajoriya12@users.noreply.github.com>
2025-03-31 15:18:25 +0530

    ..

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let sections = document.querySelectorAll("section");
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.2 });
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
